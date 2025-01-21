f1af79b55a86800bf1b7ff670aca1434724dda76
mannu-cyber <sahanimannu844@gmail.com>
2025-01-21 09:58:20 +0530

    day 1

---
+++ dsa/day-1/index.js
@@ -0,0 +1,50 @@
+// sum of all the numbers from the given array
+
+
+// const numbers = [2, 4, 7]
+
+
+// const bigNumber = numbers.reduce((total, index) => {
+
+
+//   return total + index
+
+// },0)
+// console.log(bigNumber);
+
+// console.log(bigNumber);
+
+
+
+// const num = [4, , "kaho", 1345, 3, '0', 2, 12, 5]
+
+
+// const bigNumber = num.reduce((accumulator, current) => {
+//   return current > accumulator ? current : accumulator
+// }, 0)
+
+
+// const numbers = num.filter((current, num) => {
+
+//   if (typeof (current) === "number") {
+//     return true
+//   }
+
+// })
+
+// console.log(numbers);
+
+
+
+
+
+// const numA = 50
+// const numB = 40
+// const numC = 65
+
+// function allNum(a, b, c) {
+
+//   return Math.min(a, b, c)
+// }
+// console.log(allNum(numA, numB, numC));
+
